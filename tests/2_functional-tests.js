9910e18f67189b8400e684b5dcade88b1cbe84be
Fernando <palhares.f@gmail.com>
2025-02-10 16:51:44 -0300

    All tests added and working

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,100 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
+    test('Viewing one stock', function (done) {
+        chai.request(server)
+            .get('/api/stock-prices/?stock=GOOG')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                  stockData: {
+                    stock: "GOOG",
+                    price: 187.14,
+                    likes: 0,
+                  },
+                });
+                done();
+            });
+    });
+
+    test('Viewing one stock and liking it', function (done) {
+        chai.request(server)
+            .get('/api/stock-prices/?stock=GOOG&like=true')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                  stockData: {
+                    stock: "GOOG",
+                    price: 187.14,
+                    likes: 1,
+                  },
+                });
+                done();
+            });
+    });
+
+    test('Viewing the same stock and liking it again', function (done) {
+        chai.request(server)
+            .get('/api/stock-prices/?stock=GOOG&like=true')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                  stockData: {
+                    stock: "GOOG",
+                    price: 187.14,
+                    likes: 1,
+                  },
+                });
+                done();
+            });
+    });
+
+    test('Viewing two stocks', function (done) {
+        chai.request(server)
+            .get('/api/stock-prices/?stock=GOOG&stock=MSFT')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                  stockData: [
+                    {
+                      stock: "GOOG",
+                      price: 187.14,
+                      rel_likes: 1,
+                    },
+                    {
+                      stock: "MSFT",
+                      price: 409.75,
+                      rel_likes: -1,
+                    },
+                  ],
+                });
+                done();
+            });
+    });
+
+    test('Viewing two stocks and liking them', function (done) {
+        chai.request(server)
+            .get('/api/stock-prices/?stock=GOOG&stock=MSFT&like=true')
+            .end(function (err, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                  stockData: [
+                    {
+                      stock: "GOOG",
+                      price: 187.14,
+                      rel_likes: 0,
+                    },
+                    {
+                      stock: "MSFT",
+                      price: 409.75,
+                      rel_likes: 0,
+                    },
+                  ],
+                });
+                done();
+            });
+    });
 
 });
+
